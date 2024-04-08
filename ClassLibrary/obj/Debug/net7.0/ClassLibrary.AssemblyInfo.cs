bf44b04baaaf44eba543d0140a0e5e40eaa1c78f
Miebi <User@DESKTOP-1EK6ASL>
2024-04-07 17:59:05 -0700

    Changes made to the Class library project

--- ClassLibrary/obj/Debug/net7.0/ClassLibrary.AssemblyInfo.cs
+++ ClassLibrary/obj/Debug/net7.0/ClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e92487eb8879310c38fd72951f96a6e201198e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
