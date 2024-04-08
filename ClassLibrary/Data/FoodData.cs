ed4aaf077affb1ad294c42932ac9f52f78f347f7
Miebi <User@DESKTOP-1EK6ASL>
2024-04-07 17:54:48 -0700

    Added Food DATA CLASS

---
+++ ClassLibrary/Data/FoodData.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary.Db;
+
+namespace ClassLibrary.Data
+{
+    public class FoodData
+    {
+        private readonly IDataAccess _dataAccess;
+        private readonly ConnectionStringData _connectionString;
+
+        public FoodData(IDataAccess dataAccess, ConnectionStringData connectionString)
+        {
+            _dataAccess = dataAccess;
+            _connectionString = connectionString;
+        }
+    }
+}
